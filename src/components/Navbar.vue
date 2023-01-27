6860aa0da54d88623635a3de95013f972a5b0d2e
Melaine Gérard <pro@melaine-gerard.fr>
2023-01-27 12:25:45 +0100

    :sparkles: Début de la création des composants

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+  <div class="rounded-md shadow-sm mt-4 mb-4">
+    <router-link to="/"
+                 class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+      Accueil
+    </router-link>
+    <router-link to="/cities"
+                 class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+      Villes
+    </router-link>
+    <router-link to="/about"
+                 class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+      A Propos
+    </router-link>
+  </div>
+</template>
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-import type {App} from "vue";
-import Title from './Title.vue'
-export function registerComponents(app: App) {
-    app.component('title', Title)
-}
